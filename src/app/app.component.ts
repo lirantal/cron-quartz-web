ba9bab9c60f8187a4545e9c395abd7b882fde639
Liran Tal <liran.tal@gmail.com>
2016-10-02 06:25:36 +0000

    main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'CRON TO QUARTZ';
+  subtitle = 'an open source javascript conversion library';
 }
